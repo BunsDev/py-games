43237e891bb568192fe04cce86423aaaa3175ab0
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-06 14:42:58 +0200

    Changed the names of some variables

--- Password_Generator/main.py
+++ Password_Generator/main.py
@@ -5,19 +5,19 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+letters_requested = int(input("How many letters would you like in your password?\n"))
+symbols_requested = int(input(f"How many symbols would you like?\n"))
+numbers_requested = int(input(f"How many numbers would you like?\n"))
 
 password_list = []
 
-for letter in range(0, nr_letters):
+for letter in range(0, letters_requested):
     password_list.append(random.choice(string.ascii_letters))
 
-for symbol in range(0, nr_symbols):
+for symbol in range(0, symbols_requested):
     password_list.append(random.choice(symbols))
 
-for number in range(0, nr_numbers):
+for number in range(0, numbers_requested):
     password_list.append(random.choice(numbers))
 
 random.shuffle(password_list)

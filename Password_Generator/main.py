5a62e34dd46058e66e1121fd76cf2e2c6d43165f
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-06 14:40:32 +0200

    Upgraded the complexity of the passwords

--- Password_Generator/main.py
+++ Password_Generator/main.py
@@ -1,9 +1,6 @@
 import random
 import string
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -12,18 +9,22 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-generated_password = ''
+password_list = []
 
 for letter in range(0, nr_letters):
-    randomLetter = random.choice(string.ascii_letters)
-    generated_password += randomLetter
+    password_list.append(random.choice(string.ascii_letters))
 
-for letter in range(0, nr_symbols):
-    random_symbol = random.choice(symbols)
-    generated_password += random_symbol
+for symbol in range(0, nr_symbols):
+    password_list.append(random.choice(symbols))
 
-for letter in range(0, nr_numbers):
-    random_num = random.choice(numbers)
-    generated_password += random_num
+for number in range(0, nr_numbers):
+    password_list.append(random.choice(numbers))
 
-print(f'Your password is {generated_password}')
+random.shuffle(password_list)
+
+password = ''
+
+for char in password_list:
+    password += char
+
+print(f'Your password is {password}')

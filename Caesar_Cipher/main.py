0094fa8b2ac6890fa24d2514653a864b4fb647eb
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-07 18:42:10 +0200

    Separated the code, so it can be more readable. Finished the decode algorithm

--- Caesar_Cipher/main.py
+++ Caesar_Cipher/main.py
@@ -9,16 +9,7 @@ if direction != 'encode' and direction != 'decode':
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
-def encrypt(plain_text, shift_amount):
-    message = ''
-    for i in range(len(plain_text)):
-        current_char = plain_text[i]
-        index = util.alphabet.index(current_char)
-        index += shift_amount
-        new_char = util.alphabet[index]
-        message += new_char
-    print(message)
-
-
-encrypt(plain_text=text, shift_amount=shift)
+if direction == 'encode':
+    util.encrypt(text, shift)
+elif direction == 'decode':
+    util.decrypt(text, shift)

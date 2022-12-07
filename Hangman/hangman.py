ecc8d652517213e5d88804f1b88eeb02251ccf0a
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-07 14:10:13 +0200

    Replacing '_' with letters when the letter is guessed correctly

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -5,10 +5,16 @@ random_word = random.choice(util.word_list)
 
 guessed_letter = input('Guess a letter:\n ').lower()
 
-for letter in random_word:
-    if letter == guessed_letter:
-        print('match')
-    else:
-        print('wrong')
+display = []
+word_length = len(random_word)
+
+for _ in range(word_length):
+    display += "_"
+print(display)
+
+
+for index in range(word_length):
+    if random_word[index] == guessed_letter:
+        display[index] = guessed_letter
 
 print(f'The word was {random_word}')

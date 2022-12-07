872e55148e39f9df182e88286622b0c32c2a3a0a
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-07 21:13:06 +0200

    Finished the game!

--- Caesar_Cipher/main.py
+++ Caesar_Cipher/main.py
@@ -3,24 +3,24 @@ import art
 
 print(art.logo)
 
-while True:
+should_continue = True
+while should_continue:
 
-    continue_using = input('If you want to continue using the program, press "yes", else press "no"\n')
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
-    if continue_using == 'yes':
-
-        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-
-        if direction != 'encode' and direction != 'decode':
-            print('Invalid input')
-            exit()
-
-        text = input("Type your message:\n").lower()
-        shift = int(input("Type the shift number:\n"))
-
-        shift = shift % 26
-
-        util.caesar(text, shift, direction)
-    elif continue_using == 'no':
-        print('Goodbye')
+    if direction != 'encode' and direction != 'decode':
+        print('Invalid input')
         exit()
+
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    util.caesar(text, shift, direction)
+
+    ask_continue_using = input('Would you like to start again? Type "yes" to continue, else type "no"\n')
+
+    if ask_continue_using == 'no':
+        print('Goodbye')
+        should_continue = False

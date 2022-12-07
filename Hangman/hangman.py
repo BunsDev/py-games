63453e8a38c8d575b84c42a423042a5d93e384d9
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-07 14:56:41 +0200

    Continue implementing the game

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -3,18 +3,26 @@ import util
 
 random_word = random.choice(util.word_list)
 
-guessed_letter = input('Guess a letter:\n ').lower()
-
+end_of_game = False
 display = []
 word_length = len(random_word)
 
 for _ in range(word_length):
     display += "_"
-print(display)
+print(f'The word you need to guess is {word_length} letters long:\n{display}')
 
+while not end_of_game:
+    guessed_letter = input('Guess a letter:\n').lower()
 
-for index in range(word_length):
-    if random_word[index] == guessed_letter:
-        display[index] = guessed_letter
+    for index in range(word_length):
+        letter = random_word[index]
+        if letter == guessed_letter:
+            display[index] = guessed_letter
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win")
+    else:
+        print(display)
 
 print(f'The word was {random_word}')

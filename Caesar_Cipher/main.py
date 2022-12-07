c7c18e00f99fed206b63be0304f0fe10affdb7ae
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-07 18:28:40 +0200

    Finished the encode functionality

--- Caesar_Cipher/main.py
+++ Caesar_Cipher/main.py
@@ -2,7 +2,7 @@ import util
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
-if direction != 'encode' and 'decode':
+if direction != 'encode' and direction != 'decode':
     print('Invalid input')
     exit()
 
@@ -10,15 +10,15 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(text_param, shift_param):
+def encrypt(plain_text, shift_amount):
     message = ''
-    for index in range(len(text_param)):
-        current_char = text_param[index]
+    for i in range(len(plain_text)):
+        current_char = plain_text[i]
         index = util.alphabet.index(current_char)
-        index += shift_param
+        index += shift_amount
         new_char = util.alphabet[index]
         message += new_char
     print(message)
 
 
-encrypt(text, shift)
+encrypt(plain_text=text, shift_amount=shift)

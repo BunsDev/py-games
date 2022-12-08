9cb6bc0a38093b1f63988df7353155446550fb5e
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-12-08 17:32:33 +0200

    Fix bugs

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -2,18 +2,38 @@ from util import operations
 from art import logo
 from replit import clear
 
+list_with_operations = ['+', '-', '*', '/']
+
 
 def calculator():
+    global num1, num2
     print(logo)
 
-    num1 = float(input("What's the first number?: "))
+    try:
+        num1 = float(input("What's the first number?: "))
+
+    except ValueError:
+        print('The provided value is not a number')
+        calculator()
+
     for symbol in operations:
         print(symbol)
     should_continue = True
 
     while should_continue:
         operation_symbol = input("Pick an operation: ")
-        num2 = float(input("What's the next number?: "))
+
+        if operation_symbol not in list_with_operations:
+            print('Incorrect symbol')
+            continue
+
+        try:
+            num2 = float(input("What's the next number?: "))
+
+        except ValueError:
+            print('The provided value is not a number')
+            calculator()
+
         calculation_function = operations[operation_symbol]
         answer = calculation_function(num1, num2)
         print(f"{num1} {operation_symbol} {num2} = {answer}")
